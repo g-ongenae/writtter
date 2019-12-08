21eb8e311a3f0a063f695368a20dbb84b1513dd4
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2019-12-08 19:38:33 +0100

    Updates

---
+++ precache-manifest.e19c3f3737d3f91074186f369eaf1eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0603f1482aa07c2736788ea10de68543",
+    "url": "/writtter/index.html"
+  },
+  {
+    "revision": "118bfd426cb7745a3557",
+    "url": "/writtter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b1e4b4b44556566f074",
+    "url": "/writtter/static/js/2.9bcc5c23.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/writtter/static/js/2.9bcc5c23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "118bfd426cb7745a3557",
+    "url": "/writtter/static/js/main.4d5d218e.chunk.js"
+  },
+  {
+    "revision": "a811a841c42d12458af3",
+    "url": "/writtter/static/js/runtime-main.521667d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/writtter/static/media/logo.5d5d9eef.svg"
+  }
+]);
